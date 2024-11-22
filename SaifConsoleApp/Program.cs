341b6fe242151bd0ad5c85cc0069cf1b467db107
Tabish Ahmad <tabish0211@hotmail.com>
2024-11-22 22:21:42 +0530

    Decision controls code

--- SaifConsoleApp/Program.cs
+++ SaifConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SaifConsoleApp
+﻿using System.Diagnostics;
+using System.Net.Cache;
+
+namespace SaifConsoleApp
 {
     internal class Program
     {
@@ -9,18 +12,40 @@
         
         static void Main()
         {
-            //WAP swap the two variables using 3rd varaiable and without variable
-            int x = 10, y = 20;
-            Console.WriteLine($"Before swapping x={x} and y={y}");
-            //int temp;
-            //temp = x;
-            //x = y;
-            //y = temp;
-            x = x + y;//30
-            y = x - y;//30-20=10
-            x = x - y;//30-10=20
-            Console.WriteLine($"After swapping x={x} and y={y}");
-            //assignment using--xor gate--
+            //who is eldest amongts three brother
+            Console.WriteLine("enter the age of A");
+            int ageA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the age of B");
+            int ageB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the age of C");
+            int ageC = Convert.ToInt32(Console.ReadLine());
+           
+            if (ageA > ageB) {
+                if (ageA>ageC)
+                {
+                    Console.WriteLine( "A is eldest one");
+                }
+                else
+                {
+                    Console.WriteLine("C is eldest one");
+                }
+
+            
+
+            }
+
+            else
+            {
+                if (ageB > ageC)
+                {
+                    Console.WriteLine("B is eldest one");
+                }
+                else
+                {
+                    Console.WriteLine("C is eldest one");
+                }
+
+            }
             Console.ReadLine();
 
         }
