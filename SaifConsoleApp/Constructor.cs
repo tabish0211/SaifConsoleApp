43109cd8e75a2498070c8deb2d8aec36f9ba808b
Tabish Ahmad <tabish0211@hotmail.com>
2024-12-24 23:27:29 +0530

    Constructor

---
+++ SaifConsoleApp/Constructor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaifConsoleApp
+{
+    internal class Customer
+    {
+        private string _name;
+        private string email;
+        
+        //deafult--there is no parameter
+
+        //Overloading Constructor
+        //especial method whose name is same as that of class name 
+        //to initialized class memeber at the time of object creation
+        public Customer() {
+            Console.WriteLine("This is a default constructor");
+        }
+
+        //parametrized constructor
+        public Customer(string name)
+        {
+            _name = name;
+          //  Console.WriteLine($"Parametrized constructor {_name}");
+        }
+
+        public Customer(string name,string email): this(name)
+        {
+            //_name = name;//DRY---Do not repeat yourself
+            this.email = email;
+            Console.WriteLine("parametrized constructro called");
+        }
+
+        static Customer()
+        {
+            Console.WriteLine("If you want to do some sort of configuration even before actual object creation use static constructor");
+
+        }
+
+        //copy constructor
+        public Customer CopyConstructor() {
+
+            // Customer customerCopy = new Customer(customer._name,customer.email);
+            Customer customerCopy=(Customer)this.MemberwiseClone();            
+            return customerCopy;
+
+        }
+
+        public void Display()
+        { 
+        
+
+        
+        }
+
+    }
+
+    class Caller
+    {
+
+        static void Main()
+        { 
+        
+            Customer customer = new Customer("Ramesh","ramesh@gmail.com");
+
+            Customer customer1 = customer.CopyConstructor();
+           
+            Console.ReadLine();
+        }
+    }
+}
--- SaifConsoleApp/EmployeeManagementService.cs
+++ SaifConsoleApp/EmployeeManagementService.cs
@@ -9,39 +9,73 @@ namespace SaifConsoleApp
     class ClientCode
     {
 
-        static void Main()
-        { 
+        //static void Main()
+        //{ 
         
-            EmployeeManagementService obj=new EmployeeManagementService();
+        //    EmployeeManagementService obj=new EmployeeManagementService();
 
-            //obj creates on stack--this is just a varaible which is pointing to below object address
-            //object--new EmployeeManagementService()--on heap
+        //    // //obj creates on stack--this is just a varaible which is pointing to below object address
+        //    // //object--new EmployeeManagementService()--on heap
 
-            //object.Methodname();
-           // (new EmployeeManagementService()).Add();
-            //(new EmployeeManagementService()).Get(2);
-            obj.Add();
-            obj.Get(1);
+        //    // //object.Methodname();
+        //    //// (new EmployeeManagementService()).Add();
+        //    // //(new EmployeeManagementService()).Get(2);
+        //    // obj.Add();
+        //    // obj.Get(1);
 
-            Console.ReadLine();
-        }
+        //    //obj.SetId(1);
+        //    //int id=obj.GetId();
+        //   // obj.Id = 1;//set
+        //   // int id = obj.Id;//get
+        //   // Console.WriteLine( obj.Id);//get
+
+
+
+        //    Console.ReadLine();
+        //}
     
     }
     internal class EmployeeManagementService
     {
+        //private int _id;
+        private string _name;
 
-        public void Add()
-        {
-            Console.WriteLine( "Hello");
+        //public void SetId(int id)
+        //{
+        //    _id = id;
+        //}
 
-        }
+        //public int GetId()
+        //{
+        //    return _id;
+        //}
 
-        public void Get(int eid)
-        {
-            Console.WriteLine("Hello world");
+        //property---C#2.0-----C# 4.0---C#13
+        //public int Id
+        //{
+        //    get { return _id; }
+        //    set { _id = value; }
+        //}
 
-        }
+        //property--C# 4.0--
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public char Gender { get; set; }
+        //public void Add()
+        //{
+        //    Console.WriteLine( "Hello");
+
+        //}
+
+        //public void Get(int eid)
+        //{
+        //    Console.WriteLine("Hello world");
+
+        //}
+
+
 
     }
 }
-}
+
