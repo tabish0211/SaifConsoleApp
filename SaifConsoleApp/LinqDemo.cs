4e2fca3fb613c964db916d69bda3d50dcd2585a9
Tabish Ahmad <tabish0211@hotmail.com>
2025-01-17 00:37:09 +0530

    Linq

---
+++ SaifConsoleApp/LinqDemo.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaifConsoleApp
+{
+    internal class LinqDemo
+    {
+        static void Main()
+        {
+            var employees = GetData();
+            //Linq 
+            var lstEmployees= employees.Where(e => e.Name == "Rakesh").ToList();
+
+            //foreach (var employee in lstEmployees)
+            //{
+            //    Console.WriteLine($"Id:{employee.Id} Name:{employee.Name} Gender:{employee.Gender}" +
+            //        $" Salary:{employee.Salary} Age:{employee.Age}");
+            //}
+
+
+            //foreach (var employee in employees.Where(e => e.Name.Contains("ul")).ToList())
+            //{
+            //    Console.WriteLine($"Id:{employee.Id} Name:{employee.Name} Gender:{employee.Gender}" +
+            //        $" Salary:{employee.Salary} Age:{employee.Age}");
+            //}
+
+            //if (employees.All(e=>e.Age<25))
+            //{
+            //    Console.WriteLine("All the employees are less 60 age");
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Not All the employees are less 60 age");
+
+            //}
+
+            //if (employees.Any(e => e.Age < 25))
+            //{
+            //    Console.WriteLine("Any one of  the employees are less 25 age");
+
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Not All the employees are less 60 age");
+
+            //}
+           // int count=employees.Count;
+           // int totalSalary=employees.Sum(e => e.Salary);
+           // int averageSal=(int)employees.Average(e => e.Salary);
+           // int maxSal = employees.Max(e => e.Salary);
+           //var employeMaxSal =employees.Where(e=>e.Salary==maxSal).ToList();
+
+            //var emp=employees.ElementAt(7);
+           // var emp=employees.ElementAtOrDefault(7);
+
+            //var emp=employees.First(e=>e.Name=="Surya");
+           // var emp = employees.FirstOrDefault(e => e.Name == "Surya");
+         //  var emp=employees.Single(e=>e.Name=="Surya");
+           // var emp = employees.SingleOrDefault(e => e.Name == "Surya");
+
+
+
+            Console.ReadLine();
+
+        }
+
+        private static List<Employee> GetData()
+        {
+            List<Employee> employees = new List<Employee>();
+            Employee employee1 = new Employee()
+            {
+                Id = 101,
+                Name = "Rakesh",
+                Gender = "Male",
+                Salary = 20000,
+                Age = 25
+            };
+
+            Employee employee2 = new Employee()
+            {
+                Id = 102,
+                Name = "Mahesh",
+                Gender = "Male",
+                Salary = 30000,
+                Age = 34
+            };
+
+            Employee employee3 = new Employee()
+            {
+                Id = 103,
+                Name = "Abdul",
+                Gender = "Male",
+                Salary = 10000,
+                Age = 23
+            };
+
+            Employee employee4 = new Employee()
+            {
+                Id = 104,
+                Name = "Maya",
+                Gender = "Female",
+                Salary = 29000,
+                Age = 37
+            };
+            Employee employee5 = new Employee()
+            {
+                Id = 105,
+                Name = "Sana",
+                Gender = "Female",
+                Salary = 50000,
+                Age = 42
+            };
+
+            Employee employee6 = new Employee()
+            {
+                Id = 106,
+                Name = "Danish",
+                Gender = "Male",
+                Salary = 50000,
+                Age = 45
+            };
+
+            employees.Add(employee1);
+            employees.Add(employee2);
+            employees.Add(employee3);
+            employees.Add(employee4);
+            employees.Add(employee5);
+            employees.Add(employee6);
+
+            return employees;
+
+            
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
--- SaifConsoleApp/ListDemo.cs
+++ SaifConsoleApp/ListDemo.cs
@@ -8,75 +8,75 @@ namespace SaifConsoleApp
 {
     internal class ListDemo
     {
-        static void Main()
-        {
+        //static void Main()
+        //{
 
-            //Student student1 = new Student();
-            //student1.RollNumber = 1001;
-            //student1.Name = "Ramesh";
-            //student1.Age = 30;
-
-
-            //Student student2 = new Student();
-            //student2.RollNumber = 1002;
-            //student2.Name = "Mahesh";
-            //student2.Age = 20;
+        //    //Student student1 = new Student();
+        //    //student1.RollNumber = 1001;
+        //    //student1.Name = "Ramesh";
+        //    //student1.Age = 30;
 
 
-            //Student student3 = new Student();
-            //student3.RollNumber = 1003;
-            //student3.Name = "Suresh";
-            //student3.Age = 10;
-
-            //List<Student> students = new List<Student>();
-            //students.Add(student1);
-            //students.Add(student2);
-            //students.Add(student3);
-
-            ////display
-            //foreach (var student in students)
-            //{
-            //    Console.WriteLine($"Roll Number={student.RollNumber} Name={student.Name} Age={student.Age}");
-            //}
+        //    //Student student2 = new Student();
+        //    //student2.RollNumber = 1002;
+        //    //student2.Name = "Mahesh";
+        //    //student2.Age = 20;
 
 
-            //Dictionary<string,int> populationDict= new Dictionary<string,int>();
-            //populationDict.Add("India", 10000);            
-            //populationDict.Add("China", 20000);
-            //populationDict.Add("Singapore", 30000);
+        //    //Student student3 = new Student();
+        //    //student3.RollNumber = 1003;
+        //    //student3.Name = "Suresh";
+        //    //student3.Age = 10;
 
-            //foreach (var item in populationDict)
-            //{
-            //    Console.WriteLine( $"Country={item.Key} and Population={item.Value}");
-            //}
-            //int z;
-            //int q;
-            //int x=1;
-            //int y = 2;
-            //Console.WriteLine($"Before passing to sum the value x={x} and y={y}");
-            //Sum(ref x,ref y,out z,out q);
-            //Console.WriteLine($"After passing to sum the value x={x} and y={y}");
-            //Console.WriteLine($"z={z}");
-            //Console.WriteLine($"q={q}");
-            //Console.ReadLine();
+        //    //List<Student> students = new List<Student>();
+        //    //students.Add(student1);
+        //    //students.Add(student2);
+        //    //students.Add(student3);
 
-            //(int id, string name) = GetData();
-            //Console.WriteLine(id);
-            //Console.WriteLine(name);
-
-            ClassRoom cls=new ClassRoom(80);
-            cls.DisplayCount();
-            //cls.Count = 50;
-            //ClassRoom.Count = 30;
-            //Console.WriteLine( ClassRoom.Count);
-            Console.WriteLine( cls.Count);
-            //cls.Count = 30;
-            cls.DisplayCount();
-            Console.ReadLine();
+        //    ////display
+        //    //foreach (var student in students)
+        //    //{
+        //    //    Console.WriteLine($"Roll Number={student.RollNumber} Name={student.Name} Age={student.Age}");
+        //    //}
 
 
+        //    //Dictionary<string,int> populationDict= new Dictionary<string,int>();
+        //    //populationDict.Add("India", 10000);            
+        //    //populationDict.Add("China", 20000);
+        //    //populationDict.Add("Singapore", 30000);
 
-        }
+        //    //foreach (var item in populationDict)
+        //    //{
+        //    //    Console.WriteLine( $"Country={item.Key} and Population={item.Value}");
+        //    //}
+        //    //int z;
+        //    //int q;
+        //    //int x=1;
+        //    //int y = 2;
+        //    //Console.WriteLine($"Before passing to sum the value x={x} and y={y}");
+        //    //Sum(ref x,ref y,out z,out q);
+        //    //Console.WriteLine($"After passing to sum the value x={x} and y={y}");
+        //    //Console.WriteLine($"z={z}");
+        //    //Console.WriteLine($"q={q}");
+        //    //Console.ReadLine();
+
+        //    //(int id, string name) = GetData();
+        //    //Console.WriteLine(id);
+        //    //Console.WriteLine(name);
+
+        //    ClassRoom cls=new ClassRoom(80);
+        //    cls.DisplayCount();
+        //    //cls.Count = 50;
+        //    //ClassRoom.Count = 30;
+        //    //Console.WriteLine( ClassRoom.Count);
+        //    Console.WriteLine(cls.Count);
+        //    //cls.Count = 30;
+        //    cls.DisplayCount();
+        //    Console.ReadLine();
+
+
+
+        //}
 
         public static void Sum(ref int x,ref int y,out int z,out int q)
         {
